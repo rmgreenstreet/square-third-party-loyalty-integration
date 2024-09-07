37fb40a31e61e7bd1c7c593b650dbfa5e98213b9
Robert Greenstreet <robertgreenstreet@gmail.com>
2024-09-07 11:17:50 -0500

    Add (probably redundant) error handling to connectToMongoose

--- src/utils/connectToMongoose.mjs
+++ src/utils/connectToMongoose.mjs
@@ -4,16 +4,18 @@ import { winstonLogger as logger } from "../utils/loggingSetup.mjs";
 import ApplicationError from "../models/ApplicationError.mjs";
 import { exponentialBackoff } from './exponentialBackoff.mjs';
 
-import User from '../models/User.mjs';
-
-
-const maxRetries = 5; // Number of attempts
-
 async function connectToMongoose() {
-  exponentialBackoff(async () => {
-    logger.debug("Entering connectToMongoose");
-    await mongoose.connect(process.env.DB_CONNECTION_STRING, { dbName: process.env.DB_NAME });
-  }, 10, 250);
+  logger.debug("Entering connectToMongoose");
+  try {
+    exponentialBackoff(async () => {
+      await mongoose.connect(process.env.DB_CONNECTION_STRING, { dbName: process.env.DB_NAME });
+    }, 10, 250);
+  } catch (err) {
+        // TODO Figure out how to do this more gracefully and potentially retry to start the process
+        const mongooseConnectionError = new ApplicationError({name: "MongooseConnectionError", err});
+        logger.error(mongooseConnectionError);
+        throw mongooseConnectionError;
+  }
 }
 
 export default connectToMongoose;
