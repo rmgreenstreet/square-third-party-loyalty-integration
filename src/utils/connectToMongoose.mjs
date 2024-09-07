b10da0ab3f2e15e6a351d7ec9b8d5bffc5699f8e
Robert Greenstreet <robertgreenstreet@gmail.com>
2024-09-07 10:52:26 -0500

    Fix exponential backoff for mongoose connection

--- src/utils/connectToMongoose.mjs
+++ src/utils/connectToMongoose.mjs
@@ -5,13 +5,13 @@ import User from '../models/User.mjs';
 
 const maxRetries = 5; // Number of attempts
 
-async function connectToMongoose(delay = 250) { // Default delay value
+async function connectToMongoose(delay = 250) {
+  let nextDelay = delay;
   let attempts = 0;
   logger.debug("Entering connectToMongoose");
 
   const connect = async () => {
     attempts++;
-
     try {
       const mongooseConnection = await mongoose.connect(process.env.DB_CONNECTION_STRING, { dbName: process.env.DB_NAME });
       logger.debug(`Mongoose Connected to MongoDB`);
@@ -19,9 +19,9 @@ async function connectToMongoose(delay = 250) { // Default delay value
     } catch (err) {
       logger.error(`Failed to connect to MongoDB (attempt ${attempts}): ${err.message}`);
       if (attempts < maxRetries) {
-        const nextDelay = delay * 2; // Exponential backoff
+        nextDelay *= 2; // Exponential backoff
         logger.debug(`Retrying in ${nextDelay / 1000} seconds...`);
-        setTimeout(() => connect(), nextDelay); // Retry connection
+        setTimeout(connect, nextDelay); // Retry connection
       } else {
         console.error('Max retries reached. Exiting...');
         process.exit(1); // Exit with failure code
@@ -29,7 +29,7 @@ async function connectToMongoose(delay = 250) { // Default delay value
     }
   };
 
-  connect(); // Initial call to start the connection process
+  setTimeout(connect, delay); // Initial call to start the connection process
 }
 
 export default connectToMongoose;
