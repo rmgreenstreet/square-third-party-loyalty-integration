162779b2c513f98b0d2359d764cc56667b38eaca
Robert Greenstreet <robertgreenstreet@gmail.com>
2024-09-08 09:37:54 -0500

    Update logging and error handling for squareUtils

--- src/utils/squareUtils.mjs
+++ src/utils/squareUtils.mjs
@@ -3,6 +3,7 @@ import { Client, Environment } from 'square';
 import { encrypt, decrypt } from './tokenUtils.mjs';
 import { winstonLogger as logger } from "../utils/loggingSetup.mjs";
 import ApplicationError from "../models/ApplicationError.mjs";
+import { exponentialBackoff } from './exponentialBackoff.mjs';
 
 
 // Initialize Square client
@@ -11,60 +12,80 @@ const environment = process.env.NODE_ENV === 'production'
   : Environment.Sandbox;
 
 export const createSquareClient = async (accessToken) => {
-  return new Client({
-    environment,
-    accessToken: accessToken
-  });
+  logger.debug("Creating Square Client");
+  try {
+    exponentialBackoff(async () => {
+      return await new Client({
+        environment,
+        accessToken: accessToken
+      });
+    });
+  } catch (err) {
+    // TODO Figure out how to do this more gracefully and potentially retry to start the process
+    const squareClientSetupError = new ApplicationError({ name: "SquareClientSetupError", err });
+    logger.error(squareClientSetupError);
+    throw squareClientSetupError;
+  }
 };
 
 // Function to check if the user's Square account is authorized
 export const isSquareAuthorized = async (user) => {
-  console.log("entering isSquareAuthorized");
+  logger.debug("entering isSquareAuthorized");
   if (!user.squareAccessToken || !user.squareRefreshToken) {
-    console.log("User does not have square access token");
+    logger.debug("User does not have square access token");
     return false;
   }
-  console.log("User has square access token, attempting to decrypt and use it");
+  logger.debug("User has square access token, attempting to decrypt and use it");
   try {
-    console.log("Initializing Square Client");
+    logger.debug("Initializing Square Client");
     const client = await createSquareClient(decrypt(user.squareAccessToken));
     const { customersApi } = client;
-    console.log("Square Client Initialized, customersApi:", customersApi);
-    console.log("Attempting to retrieve customers")
+    // console.log("Square Client Initialized, customersApi:", customersApi);
+    logger.debug("Attempting to retrieve customers")
     // Attempt to make an API call to check if the token is valid
-    const response = await customersApi.listCustomers();
-    console.log("customersApi response.result:", response.result);
-    return response.result ? true : false;
+    const response = exponentialBackoff(async () => { await customersApi.listCustomers(); })
+    logger.debug("customersApi response.result:", response.result);
+    const isAuthorized = response.result ? true : false;
+    logger.info(`User is Square Authorized: ${isAuthorized}`, { user: user.id });
+    return isAuthorized
   } catch (error) {
-    console.error('Error checking Square authorization:', error);
+    // TODO Figure out how to do this more gracefully and potentially retry to start the process
+    const isSquareAuthorizedError = new ApplicationError({ name: "IsSquareAuthorizedError", err });
+    logger.error(isSquareAuthorizedError);
+    req.flash("error", "There was an issue verifying Square authorization");
     return false;
   }
 };
 
 // Refresh access token using the refresh token and save the tokens
 export const refreshAccessToken = async (user) => {
+  logger.debug("Entering refreshAccessToken");
   try {
-    const client = new Client({
-      environment: process.env.SQUARE_ENVIRONMENT || 'sandbox',
-    });
-
+    const client = createSquareClient(decrypt(user.accessToken));
     const { oAuthApi } = client;
-    const response = await oAuthApi.obtainToken({
-      grant_type: 'refresh_token',
-      refresh_token: decrypt(user.squareRefreshToken),
-      client_id: process.env.SQUARE_APP_ID,
-      client_secret: process.env.SQUARE_APP_SECRET,
+
+    logger.debug("Attempting to obtain new accessToken with current refreshToken")
+    const response = await exponentialBackoff(async () => {
+      oAuthApi.obtainToken({
+        grant_type: 'refresh_token',
+        refresh_token: decrypt(user.squareRefreshToken),
+        client_id: process.env.SQUARE_APP_ID,
+        client_secret: process.env.SQUARE_APP_SECRET,
+      });
     });
 
     if (response.result.accessToken && response.result.refreshToken) {
+      logger.debug("Token refresh successful. Attempting to save new tokens");
       user.squareAccessToken = encrypt(response.result.accessToken);
       user.squareRefreshToken = encrypt(response.result.refreshToken);
       await user.save(); // Save updated tokens to the user
+      logger.info("Token Refresh successful", {user: user.id})
     }
-
     return true;
   } catch (error) {
-    console.error('Error refreshing Square access token:', error);
+    // TODO Figure out how to do this more gracefully and potentially retry to start the process
+    const SquareTokenRefreshError = new ApplicationError({ name: "SquareTokenRefreshError", err });
+    logger.error(SquareTokenRefreshError);
     return false;
   }
 };
